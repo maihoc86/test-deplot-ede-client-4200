bb8de3dc0ea87b2fa7ddcb88ceffe4c5c68742a3
haminhsang1903 <haminhsang1903@gmail.com>
2021-07-31 13:08:40 +0700

    init

---
+++ ede-customer-service/src/main/java/com/ede/edecustomerservice/EdeCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ede.edecustomerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdeCustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdeCustomerServiceApplication.class, args);
+	}
+
+}
---
+++ ede-gateway/src/main/java/com/ede/edegateway/EdeGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ede.edegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdeGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdeGatewayApplication.class, args);
+	}
+
+}
