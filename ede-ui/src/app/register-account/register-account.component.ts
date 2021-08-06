c21a1f1052e30f45ee7e32030e8e0aa592e21721
Thái Hoàng Mai Học <maihoc348@gmail.com>
2021-08-06 15:26:41 +0700

    validate form register customer-service

--- ede-ui/src/app/register-account/register-account.component.ts
+++ ede-ui/src/app/register-account/register-account.component.ts
@@ -14,25 +14,26 @@ import { CustomerServiceService } from '../Services/register-service';
   styleUrls: ['./register-account.component.css'],
 })
 export class RegisterAccountComponent implements OnInit {
+  genders = [
+    new Genders('N', 'Nam'),
+    new Genders('NU', 'Nữ'),
+    new Genders('K', 'Khác'),
+  ];
   public register = new FormGroup({
-    username: new FormControl('', Validators.required),
-    password: new FormControl('', Validators.required),
-    first_name: new FormControl('', Validators.required),
-    address: new FormControl('', Validators.required),
-    last_name: new FormControl('', Validators.required),
+    username: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9_-]{6,50}$")]),
+    password: new FormControl('', [Validators.required, Validators.pattern("^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\\s).{8,50}$")]),
+    first_name: new FormControl('', [Validators.required, Validators.pattern("^\\S([a-zA-Z\\xC0-\\uFFFF]{0,}[ \\-\\']{0,}){1,}$")]),
+    address: new FormControl('', [Validators.required, Validators.pattern("^\\S([a-zA-Z0-9\\xC0-\\uFFFF\\.]{0,}[ \\-\\' \\.-]{0,}){1,}$")]),
+    last_name: new FormControl('', [Validators.required, Validators.pattern("^\\S([a-zA-Z0-9\\xC0-\\uFFFF\\.]{0,}[ \\-\\' \\.-]{0,}){1,}$")]),
     gender: new FormControl('', Validators.required),
     photo: new FormControl(null),
-    email: new FormControl('', [Validators.required,Validators.email]),
+    email: new FormControl('', [Validators.required, Validators.email]),
     is_delete: new FormControl(false),
     role: new FormControl('US'),
     otp: new FormControl(null),
-    phone: new FormControl('', Validators.required),
+    phone: new FormControl('', [Validators.required, Validators.pattern("(84|0[3|5|7|8|9])+([0-9]{8})\\b")]),
+    confirmPassword: new FormControl('', Validators.required),
   });
-  genders = [
-    new Genders('N', 'M'),
-    new Genders('NU', 'W'),
-    new Genders('K', 'D'),
-  ];
   constructor(
     private router: Router,
     private route: ActivatedRoute,
@@ -41,8 +42,27 @@ export class RegisterAccountComponent implements OnInit {
   get f(): { [key: string]: AbstractControl } {
     return this.register.controls;
   }
+  // All is this method
+  onPasswordChange() {
+    if (this.confirm_password.value == this.password.value) {
+      this.confirm_password.setErrors(null);
+    } else {
+      this.confirm_password.setErrors({ mismatch: true });
+    }
+  }
 
-  ngOnInit(): void {}
+  // getting the form control elements
+  get password(): AbstractControl {
+    return this.register.controls['password'];
+  }
+
+  get confirm_password(): AbstractControl {
+    return this.register.controls['confirmPassword'];
+  }
+  ngOnInit(): void {
+    this.genders;
+    console.log(this.genders);
+  }
   private createNewData() {
     this.register.addControl('is_active', new FormControl(false));
     const newUser: any = {};
@@ -53,7 +73,6 @@ export class RegisterAccountComponent implements OnInit {
     }
     return newUser as User;
   }
-
   public registerUser() {
     this.customerservice.addUser(this.createNewData()).subscribe((data) => {
       console.log(data);
