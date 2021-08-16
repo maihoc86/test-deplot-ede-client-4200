2ad1ed9bbce818536a07ba5a2067f2e1f07c58d6
Thái Hoàng Mai Học <maihoc348@gmail.com>
2021-08-16 21:47:06 +0700

    create parent category

--- ede-ui-admin/src/app/Services/manage-category/manage-categoty.service.ts
+++ ede-ui-admin/src/app/Services/manage-category/manage-categoty.service.ts
@@ -1,37 +1,41 @@
 import { Injectable } from '@angular/core';
-import{
+import {
   HttpHeaders,
   HttpClient,
   HttpErrorResponse,
 }
-from '@angular/common/http';
-import { Parent_Category } from 'src/app/models/Parent_category.module';
-import { Parent_Child_Category } from 'src/app/models/Parent_Child_category.module';
-import { Child_Category } from 'src/app/models/Child_category.module';
+  from '@angular/common/http';
+import { Parent_Category } from 'src/app/models/Parent_category.model';
+import { Parent_Child_Category } from 'src/app/models/Parent_Child_category.model';
+import { Child_Category } from 'src/app/models/Child_category.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ManageCategotyService {
-  private httpOptions ={
+  private httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
     }),
   };
 
 
-  private REST_API_SERVER ='http://localhost:8080/ede-product';
+  private REST_API_SERVER = 'http://localhost:8080/ede-product';
 
-  constructor(private httClient: HttpClient) { }
-  public loadParentCategory(){
-    return this.httClient.get<any>(this.REST_API_SERVER+'/view/parent_category', this.httpOptions);
+  constructor(private httpClient: HttpClient) { }
+  public loadParentCategory() {
+    return this.httpClient.get<any>(this.REST_API_SERVER + '/view/parent_category', this.httpOptions);
   }
 
-  public loadParent_Child_Category(){
-    return this.httClient.get<any>(this.REST_API_SERVER+'/view/child_parent_category', this.httpOptions);
+  public loadParent_Child_Category() {
+    return this.httpClient.get<any>(this.REST_API_SERVER + '/view/child_parent_category', this.httpOptions);
   }
 
-  public load_Child_Category(){
-    return this.httClient.get<any>(this.REST_API_SERVER+'/view/child_category', this.httpOptions);
+  public load_Child_Category() {
+    return this.httpClient.get<any>(this.REST_API_SERVER + '/view/child_category', this.httpOptions);
+  }
+
+  public addNewParentCategory(data: Parent_Category) {
+    return this.httpClient.post<any>(this.REST_API_SERVER + '/create/parent_category', data, this.httpOptions);
   }
 }
--- ede-ui-admin/src/app/manager-category/manager-category.component.ts
+++ ede-ui-admin/src/app/manager-category/manager-category.component.ts
@@ -1,17 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import Swal from 'sweetalert2';
-
-
-
 import {
   FormGroup,
   FormControl,
   Validators,
   AbstractControl,
 } from '@angular/forms';
-import { Parent_Category } from '../models/Parent_category.module';
-import { Parent_Child_Category } from '../models/Parent_Child_category.module';
-import { Child_Category } from '../models/Child_category.module';
+import { Parent_Category } from '../models/Parent_category.model';
+import { Parent_Child_Category } from '../models/Parent_Child_category.model';
+import { Child_Category } from '../models/Child_category.model';
 import { ManageCategotyService } from '../Services/manage-category/manage-categoty.service';
 @Component({
   selector: 'app-manager-category',
@@ -19,45 +16,97 @@ import { ManageCategotyService } from '../Services/manage-category/manage-catego
   styleUrls: ['./manager-category.component.css']
 })
 export class ManagerCategoryComponent implements OnInit {
+  url = "assets/images/fancy_upload.png";
+  nameImageUrl = "";
+  onselectFile(e: any) {
+    if (e.target.files) {
+      var reader = new FileReader();
+      this.nameImageUrl = e.target.files[0].name;
+      reader.readAsDataURL(e.target.files[0]);
+      reader.onload = (e: any) => {
+        this.url = e.target.result;
+      }
+    }
+  }
+  public parent = new FormGroup({
+    name: new FormControl(''),
+    image_url: new FormControl(''),
+    is_delete: new FormControl(false),
+    is_enable: new FormControl(false),
+  });
 
   constructor(
     private manageCategoryService: ManageCategotyService,
-
   ) { }
 
   ngOnInit(): void {
     this.loadParentCategory();
   }
 
-
-  public items:any=[];
-  public loadParentCategory(){
-    this.manageCategoryService.loadParentCategory().subscribe((data) =>{
-      const item = data.map(function(obj: {
+  // lấy dữ liệu từ FormControl Parent_Category về
+  private createNewDataParentCategory() {
+    const newParentCategory: any = {};
+    this.parent.controls['image_url'].setValue(this.nameImageUrl);
+    for (const controlName in this.parent.controls) {
+      if (controlName) {
+        newParentCategory[controlName] = this.parent.controls[controlName].value;
+        console.log(newParentCategory[controlName]);
+      }
+    }
+    return newParentCategory as Parent_Category;
+  }
+  // thêm mới Parent_Category
+  public addNewParentCategory() {
+    this.manageCategoryService.addNewParentCategory(this.createNewDataParentCategory()).subscribe(
+      (data) => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Thành công!',
+          text: 'Thêm loại danh mục thành công',
+          confirmButtonText: `OK`,
+        }).then((result) => {
+          if (result.isConfirmed) {
+            this.parent.reset()
+          } else {
+            this.parent.reset()
+          }
+        })
+      },
+      (err) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Lỗi',
+          text: err.error.message,
+        });
+      }
+    );
+  }
+  public items: any = [];
+  public loadParentCategory() {
+    this.manageCategoryService.loadParentCategory().subscribe((data) => {
+      const item = data.map(function (obj: {
         id: any;
         name: any;
         image_url: any;
         is_enable: any;
         is_delete: any;
-      }){
+      }) {
         return obj;
       });
       this.items = item;
     })
   }
-
-
-  public itemP:any=[];
-  public loadParent_Child_Category(){
-    this.manageCategoryService.loadParent_Child_Category().subscribe((data) =>{
-      const item = data.map(function(obj: {
+  public itemP: any = [];
+  public loadParent_Child_Category() {
+    this.manageCategoryService.loadParent_Child_Category().subscribe((data) => {
+      const item = data.map(function (obj: {
         id: any;
         name: any;
         id_parent: any;
         image_url: any;
         is_enable: any;
         is_delete: any;
-      }){
+      }) {
         return obj;
       });
       this.itemP = item;
