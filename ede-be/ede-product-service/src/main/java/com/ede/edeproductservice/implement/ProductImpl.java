1a1da670b6e10dbede2f521e049ccd4f64aaa0f5
vinh <mvinhle22@gmail.com>
2021-08-19 20:57:44 +0700

    basic back-end and database, need fix order by of jpql

--- ede-be/ede-product-service/src/main/java/com/ede/edeproductservice/implement/ProductImpl.java
+++ ede-be/ede-product-service/src/main/java/com/ede/edeproductservice/implement/ProductImpl.java
@@ -3,6 +3,8 @@ package com.ede.edeproductservice.implement;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.ede.edeproductservice.dao.ProductDao;
@@ -19,6 +21,11 @@ public class ProductImpl implements ProductService {
 	@Override
 	public List<Product> findAll() {
 		return dao.findAll();
+	}
+
+	@Override
+	public Page<Product> searchByKeysearch(String keysearch, PageRequest pageRequest) {
+		return this.dao.searchBykeysearch(keysearch, pageRequest);
 	}
 
 }
