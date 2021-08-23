a540875da0664689ce02fe21483eb699c41dac03
tranthikimthanh3005 <tranthikimthanh3005@gmail.com>
2021-08-23 12:41:04 +0700

    Commit UI Shop

--- ede-ui/src/app/shop-interface/shop-interface.component.spec.ts
+++ ede-ui/src/app/shop-interface/shop-interface.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ProductShopComponent } from './add-product-shop.component';
+import { ShopInterfaceComponent } from './shop-interface.component';
 
-describe('ProductShopComponent', () => {
-  let component: ProductShopComponent;
-  let fixture: ComponentFixture<ProductShopComponent>;
+describe('ShopInterfaceComponent', () => {
+  let component: ShopInterfaceComponent;
+  let fixture: ComponentFixture<ShopInterfaceComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProductShopComponent ]
+      declarations: [ ShopInterfaceComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ProductShopComponent);
+    fixture = TestBed.createComponent(ShopInterfaceComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
