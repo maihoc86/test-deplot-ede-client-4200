a95d252837f802436e2a5a717470829df49ba151
vit123456 <nguyenhoangviet132001@gmail.com>
2021-08-28 09:14:44 +0700

    add interceptor

---
+++ ede-be/ede-product-service/src/main/java/com/ede/edeproductservice/interceptor/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.ede.edeproductservice.interceptor;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+	@Autowired
+	LogInterceptor login;
+	  @Override
+	   public void addInterceptors(InterceptorRegistry registry) {
+	      // LogInterceptor apply to all URLs.
+	      registry.addInterceptor(login).excludePathPatterns("/");
+	   }
+}
