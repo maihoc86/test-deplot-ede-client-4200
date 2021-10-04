82d7ebdac8a9e1cf3f05d003e398c957018b2a07
tranthikimthanh3005 <tranthikimthanh3005@gmail.com>
2021-10-04 21:57:32 +0700

    bổ sung update cmt

--- ede-be/ede-product-service/src/main/java/com/ede/edeproductservice/restcontroller/user/CreateCommentRestController.java
+++ ede-be/ede-product-service/src/main/java/com/ede/edeproductservice/restcontroller/user/CreateCommentRestController.java
@@ -8,12 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ede.edeproductservice.ResponseHandler;
 import com.ede.edeproductservice.entity.Evaluate;
 import com.ede.edeproductservice.entity.Product_option;
 import com.ede.edeproductservice.service.Auth_Service;
@@ -59,22 +63,33 @@ public class CreateCommentRestController {
 		return UUID.randomUUID().toString();
 	}
 	
+
+	
 	@PostMapping("/insert/comment/user/{idUser}/{id}")
-	public Evaluate CreateCommentByIdUser( @PathVariable("idUser") String idUser, @PathVariable("id") String id, @RequestBody Evaluate comment,HttpServletResponse response ) throws IOException {
+	public ResponseEntity  CreateCommentByIdUser( @PathVariable("idUser") String idUser, @PathVariable("id") String id, @RequestBody Evaluate comment,HttpServletResponse response ) throws IOException {
 		List<Product_option> list = order_detail_service.findAllOptionProductByIduser(idUser,id);
 		if( list.size() < 1) {
-			 response.sendError(403,"Bạn chưa mua sản phẩm nào của shop?");
-			 return null;
+//			 response.sendError(403,"Bạn chưa mua sản phẩm nào của shop?");
+//			 return null;
+			return ResponseHandler.generateResponse(HttpStatus.BAD_REQUEST, true, "Bạn chưa mua sản phẩm nào của shop!", null ,null);
 		}
 		List<Evaluate> evalue = evaluate_service.findByUserAndProduct(user_service.findById(idUser), product_service.findById(id));
-		if( list.size() >= 1) {
-			 response.sendError(403,"Bạn đã comment trước đó");
-			 return null;
+		if( evalue.size() >= 1) {
+			return ResponseHandler.generateResponse(HttpStatus.FORBIDDEN, true, "Bạn đã comment trước đó !", null ,null);
 		}
 		comment.setId(generateUUID());
 		comment.setUser(user_service.findById(idUser));
 		comment.setProduct(product_service.findById(id));
-		return evaluate_service.save(comment);
+		 return ResponseEntity.ok( evaluate_service.save(comment));
+	}
+	
+	@PutMapping("/update/comment/user/{idUser}/{id}")
+	public ResponseEntity  UpdateCommentByIdUser( @PathVariable("idUser") String idUser, @PathVariable("id") String id, @RequestBody Evaluate comment,HttpServletResponse response ) {
+		List<Evaluate> evalue = evaluate_service.findByUserAndProduct(user_service.findById(idUser), product_service.findById(id));
+		Evaluate evaOld = evalue.get(0);
+		evaOld.setContent(comment.getContent());
+		evaOld.setRate(comment.getRate());
+		 return ResponseEntity.ok( evaluate_service.save(evaOld));
 	}
 	
 	
