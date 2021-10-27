66ddb333376561fea06065c8cc17de4443c0f353
Thai Hoang Mai Hoc <maihoc348@gmail.com>
2021-10-27 21:24:34 +0700

    Thiết kế giao diện & load địa chỉ

--- ede-ui/src/app/shopping-cart/shopping-cart.component.ts
+++ ede-ui/src/app/shopping-cart/shopping-cart.component.ts
@@ -1,19 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { HeaderService } from 'src/app/Services/header/header.service';
+import { ApiAddressService } from '../Services/api-address/api-address.service';
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
   styleUrls: ['./shopping-cart.component.css'],
 })
 export class ShoppingCartComponent implements OnInit {
-  constructor(private headerService: HeaderService) {
+  constructor(
+    private headerService: HeaderService,
+    private apiAddressService: ApiAddressService
+  ) {
     this.headerService.myMethod$.subscribe((data) => {
       this.cart = data;
     });
   }
+  public listCitys: any = [];
+  public listDistricts: any = [];
+  public listWards: any = [];
+  isHiddenAddress: boolean = true;
+  isHiddenWards: boolean = true;
+  isHiddenDistrict: boolean = true;
   public cart: Array<any> = [];
   public totalCart: any = 0;
   ngOnInit(): void {
+    this.getApiCity();
     this.loadCart();
   }
   loadCart() {
@@ -21,6 +33,13 @@ export class ShoppingCartComponent implements OnInit {
 
     this.cart = json ? JSON.parse(json) : [];
   }
+
+  public address = new FormGroup({
+    city: new FormControl(''),
+    district: new FormControl(''),
+    wards: new FormControl(''),
+    address: new FormControl(''),
+  });
 
   /**
    * Hàm thay đổi số lượng
@@ -80,5 +99,67 @@ export class ShoppingCartComponent implements OnInit {
       this.loadTotal();
       return true;
     }
+  }
+
+  /**
+   * Hàm lấy ra tất cả các thành phố
+   * @returns {obj} danh sách thành phố
+   */
+  public getApiCity() {
+    this.apiAddressService.getApiCity().subscribe((data) => {
+      const listCity = data.map(function (obj: {
+        id: any;
+        code: any;
+        name: any;
+      }) {
+        return obj;
+      });
+      this.listCitys = listCity;
+    });
+  }
+
+  /**
+   * Hàm lấy ra tất cả các quận theo id của thành phố
+   * @param {string} id id của thành phố
+   * @returns {obj} danh sách quận
+   */
+  public getApiDistricts(id: any) {
+    this.apiAddressService.getApiDistricts(id).subscribe((data) => {
+      const listDistrict = data.map(function (obj: { id: any; name: any }) {
+        return obj;
+      });
+      console.log(listDistrict);
+      this.listDistricts = listDistrict;
+    });
+  }
+
+  /**
+   * Hàm lấy ra tất cả các phường theo id của quận
+   * @param {string} id id của quận
+   * @returns {obj} danh sách phường
+   */
+  public getApiWards(id: any) {
+    this.apiAddressService.getApiWards(id).subscribe((data) => {
+      const listWard = data.map(function (obj: { id: any; name: any }) {
+        return obj;
+      });
+      this.listWards = listWard;
+    });
+  }
+
+  showSelectionAddress() {
+    this.address.controls['address'].setValue('');
+    this.isHiddenAddress = !this.isHiddenAddress;
+  }
+
+  getDistricts() {
+    this.isHiddenDistrict = false;
+    console.log(this.address.controls['city'].value);
+    this.getApiDistricts(this.address.controls['city'].value.id);
+  }
+  getWards() {
+    this.isHiddenWards = false;
+
+    this.getApiWards(this.address.controls['district'].value.id);
   }
 }
