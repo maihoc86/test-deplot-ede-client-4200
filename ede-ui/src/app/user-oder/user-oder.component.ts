0ba1d8344ea752b6d134ab46deb6d804790ffa5d
Thai Hoang Mai Hoc <maihoc348@gmail.com>
2021-11-14 20:54:16 +0700

    v1

--- ede-ui/src/app/user-oder/user-oder.component.ts
+++ ede-ui/src/app/user-oder/user-oder.component.ts
@@ -24,6 +24,7 @@ export class UserOderComponent implements OnInit {
   public listIdShopinOrder: any = [];
   public listOrder: Array<any> = [];
   public listOrderFilter: Array<any> = [];
+  public listIdShop: any = [];
 
   public getAllOrderUser() {
     this.headerService
@@ -60,6 +61,9 @@ export class UserOderComponent implements OnInit {
   filterOrder(value: any) {
     this.getAllOrderUser();
     this.listOrderFilter = this.listOrder = this.listOrder.filter((e) => {
+      // this.listIdShop.push({
+      //   shop: e.order_detail[0].productOption.product.shop,
+      // });
       return e.status === value;
     });
     console.log(this.listOrderFilter);
