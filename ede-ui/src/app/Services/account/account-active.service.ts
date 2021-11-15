0c21145f3891e36db9c8a64fafdb51a1865c52ad
vit123456 <nguyenhoangviet132001@gmail.com>
2021-11-15 23:45:35 +0700

    change pass

--- ede-ui/src/app/Services/account/account-active.service.ts
+++ ede-ui/src/app/Services/account/account-active.service.ts
@@ -4,20 +4,25 @@ import {
   HttpClient,
   HttpErrorResponse,
 } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
 @Injectable({
   providedIn: 'root'
 })
 export class AccountActiveService {
   private httpOptions = {
     headers: new HttpHeaders({
-      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Type': 'application/json',
+      Authorization: this.cookieService.get('auth'),
     }),
-  }; constructor(private httpClient: HttpClient) { }
+  }; constructor(private httpClient: HttpClient,    private cookieService: CookieService) { }
 
   private REST_API_SERVER = 'http://localhost:8080/ede-customer';
  
   public checkActive(email: string, token: string) {
     var body = 'email='+email+'&token='+token;
     return this.httpClient.post<any>(this.REST_API_SERVER + '/account/verify', body, this.httpOptions);
+  }
+  public changePassWord(id:any,newpass:any){
+    return this.httpClient.put<any>(this.REST_API_SERVER + '/user/changepassword/'+id, newpass, this.httpOptions);
   }
 }
--- ede-ui/src/app/user-chang-pass/user-chang-pass.component.ts
+++ ede-ui/src/app/user-chang-pass/user-chang-pass.component.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
+import { CookieService } from 'ngx-cookie-service';
+import { HeaderService } from 'src/app/Services/header/header.service';
+import { AccountActiveService } from '../Services/account/account-active.service';
 @Component({
   selector: 'app-user-chang-pass',
   templateUrl: './user-chang-pass.component.html',
   styleUrls: ['./user-chang-pass.component.css']
 })
 export class UserChangPassComponent implements OnInit {
-
-  constructor() { }
-
+  public form = new FormGroup({
+    pass: new FormControl('',[Validators.required]),
+    newpass :  new FormControl('',[Validators.required]),
+    confirm :  new FormControl('',[Validators.required]),
+  }) 
+  constructor(
+    private accountService:AccountActiveService,
+    private headerService: HeaderService,
+    private cookieService: CookieService,) { }
+  public userlogin: any;
+  public Error:any="";
   ngOnInit(): void {
+    this.getUserLogin();
   }
-
+  public getUserLogin() {
+    this.headerService
+     .getUserByToken(this.cookieService.get('auth'))
+     .toPromise()
+     .then((data) => {
+    
+       this.userlogin = data;
+       console.log(data);
+     })
+     .catch((err) => {
+       console.log(err);
+     });
+ }
+ public changePassWord(){
+   if(!this.form.valid){
+     this.Error="Vui lòng nhập đầy đủ thông tin"
+   }else if(this.userlogin.password!=this.form.controls['pass'].value){
+    this.Error="Mật khẩu hiện tại không đúng"
+   }else if(this.form.controls['newpass'].value!=this.form.controls['confirm'].value){
+    this.Error="Nhập lại mật khẩu không đúng"
+   }else{
+    this.Error="...............";
+    this.accountService.changePassWord(this.userlogin.id,this.form.controls['newpass'].value).subscribe(
+      data=>{
+        this.Error="Đổi mật khẩu thành công";
+        this.userlogin=data;
+        this.form.reset();
+      }
+    )
+   }
+ }
 }
