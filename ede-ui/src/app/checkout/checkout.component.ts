79f51eaa6ca2ce7bd2e6e6030f5fddb81d8d9b0e
Vinh <mvinhle22@gmail.com>
2021-11-26 19:01:26 +0700

    tạm thời load dữ liệu checkout

--- ede-ui/src/app/checkout/checkout.component.ts
+++ ede-ui/src/app/checkout/checkout.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Component, OnChanges, SimpleChanges, Input } from '@angular/core';
+import { OrderShopService } from '../Services/order-shop/order-shop.service';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
-export class CheckoutComponent implements OnInit {
+export class CheckoutComponent implements OnChanges {
 
-  constructor() { }
+  @Input()
+  public productList:any = []
+  public totalprice:number = 0
 
-  ngOnInit(): void {
-    
+  constructor(
+    private orderShopService: OrderShopService,
+    private router: Router,
+    private route: ActivatedRoute
+  ){
+    this.productList = JSON.parse("" + localStorage.getItem("cart")) || []
+    this.totalprice = this.productList.reduce((last:number, item:any) => (last + (item?.product_option?.price * item?.quantity)),0) 
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.totalprice = this.productList.reduce((last:number, item:any) => (last + (item?.product_option?.price * item?.quantity)),0) 
   }
 
 }
